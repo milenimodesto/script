0f6ce8469aa8e07e1da61aff0bd7dab75ac6ef0f
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2023-10-27 08:04:54 -0300

    Add files via upload

---
+++ operador.and.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 20;
+if (a < 5 && b >10 ) {
+    console.log("ambas as condições são verdadeiras");
+} else {
+ console.log("pelo menos uma das condições é falsa ou as duas estão falsas.");
+ 
+}
